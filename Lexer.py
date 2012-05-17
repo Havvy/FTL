7a44e6f43626cf0c63b05cd5ca7bc08a780c9c36
Ryan Roler <ryan.roler@gmail.com>
2012-05-16 20:07:17 -0700

    Initial commit of the start of the Lexer. Handles "@template()" currently, and will read the parse_me.txt file by default.

---
+++ Lexer.py
@@ -0,0 +1,49 @@
+"""Flux Lexer
+    requirements: Python 3
+
+    Flux is an attempt to create kinder markup language than WikiMedia's
+    WikiCode, and this Lexer is the beginning.
+
+    For more information, visit the project's wiki:
+        http://flux.referata.com/
+"""
+
+import Tokens
+
+
+def char_stream(txt):
+    """Yields a stream of characters from a given text file"""
+    with open(txt) as stream:
+        for line in stream:
+            for char in line:
+                yield char
+
+
+def flux_tokenizer(stream=char_stream, fluxor='parse_me.txt'):
+    """Reads characters from a stream
+    Characters are matched against tokens and a list
+    of tokens is returned.
+    """
+
+    previous_state = current = Tokens.FLUX_INIT()
+    token_stream = []
+
+    for char in stream(fluxor):
+        new = current.lookup(char)
+
+        if new:
+            previous_state = current
+            current = new(char)
+            token_stream.append(previous_state)
+        else:
+            current.consumed += char
+
+        #TODO: Used for presentations, remove for production
+        print(current)
+
+    token_stream.append(current)
+    token_stream.append(Tokens.EOF())
+    return token_stream
+
+if __name__ == '__main__':
+    print([str(x) for x in flux_tokenizer()])
---
+++ Tokens.py
@@ -0,0 +1,56 @@
+"""Module of token classes used in the Lexer/Parser"""
+
+
+import re
+
+
+class Token(object):
+    """Base token class
+       Each token will have a 'table' of regex keys and associated Tokens,
+       and a 'consume' string to keep track of the token's characters
+    """
+    def __init__(self, char=''):
+        self.consumed = char
+
+    def __str__(self):
+        """str(Token) => TokenName::CharsConsumed"""
+        return str(self.__class__).split('.')[-1][:-2] + '::' + self.consumed
+
+    def lookup(self, value):
+        """Compared values against regex keys in a Token's table
+           If a match is found,
+        """
+        for key in self.table.keys():
+            if re.match(key, value):
+                return self.table[key]
+
+
+class NEW_LINE(Token):
+    table = {}
+
+
+class TEMPLATE_CLOSE_PAREN(Token):
+    table = {'\n': NEW_LINE}
+
+
+class TEMPLATE_OPEN_PAREN(Token):
+    table = {"\)": TEMPLATE_CLOSE_PAREN, '\n': NEW_LINE}
+
+
+class AT_TEXT(Token):
+    table = {"\(": TEMPLATE_OPEN_PAREN, '\n': NEW_LINE}
+
+
+class AT(Token):
+    """The @ character was read"""
+    table = {"[a-zA-Z0-9 ]": AT_TEXT, '\n': NEW_LINE}
+
+
+class EOF(Token):
+    """End of file token ends each token stream"""
+    table = {}
+
+
+class FLUX_INIT(Token):
+    """The first token added to the token stream"""
+    table = {"\@": AT, '\n': NEW_LINE}
