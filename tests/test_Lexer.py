0f90098e25d1b01ea7773709c96991fb4d4ed4c9
r3 <ryan.roler@gmail.com>
2012-05-18 11:14:00 -0700

    Added Token.ARG that handles Template arguments. Test added and all tests pass. Extracted Token tables to deal with circular dependencies in the state machine. They're still in the Token file.

--- tests/test_Lexer.py
+++ tests/test_Lexer.py
@@ -11,7 +11,7 @@ from FTL import Lexer
 class TestTokenizer():
     def setup_template_stream(self):
         def stream(x):
-            return (char for char in "@template()")
+            return (char for char in "@template()\n")
         return stream
 
     def pytest_funcarg__template_stream(self, request):
@@ -19,11 +19,32 @@ class TestTokenizer():
         return request.cached_setup(self.setup_template_stream,
                 scope='function')
 
+    def setup_template_arg_stream(self):
+        def stream(x):
+            return (char for char in "@template(arg1)\n")
+        return stream
+
+    def pytest_funcarg__template_arg_stream(self, request):
+        """Create a mock character stream: @template()"""
+        return request.cached_setup(self.setup_template_arg_stream,
+                scope='function')
+
     def test_template_lexing(self, template_stream):
         tokens = Lexer.flux_tokenizer(template_stream)
         results = [str(x) for x in tokens]
         assert  results == ['FLUX_INIT::', 'AT::@', 'AT_TEXT::template',
-                'TEMPLATE_OPEN_PAREN::(', 'TEMPLATE_CLOSE_PAREN::)', 'EOF::']
+                            'TEMPLATE_OPEN_PAREN::(',
+                            'TEMPLATE_CLOSE_PAREN::)',
+                            'NEW_LINE::\n', 'EOF::']
+
+    def test_template_arg_lexing(self, template_arg_stream):
+        tokens = Lexer.flux_tokenizer(template_arg_stream)
+        results = [str(x) for x in tokens]
+        print(results)
+        assert results == ['FLUX_INIT::', 'AT::@', 'AT_TEXT::template',
+                           'TEMPLATE_OPEN_PAREN::(', 'ARG::arg1',
+                           'TEMPLATE_CLOSE_PAREN::)', 'NEW_LINE::\n',
+                           'EOF::']
 
 
 class TestCharStream():
