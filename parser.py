3af8f524e3cc9ba3dda232afbf98deebeaa9c108
Havvy <ryan.havvy@gmail.com>
2012-06-17 16:15:12 -0700

    Created parser.py. No actual content yet.

---
+++ parser.py
@@ -0,0 +1,12 @@
+"""Flux Lexer
+    requirements: Python 3
+
+    Flux is an attempt to create kinder markup language than WikiMedia's
+    WikiCode, and this Lexer is the beginning.
+
+    For more information, visit the project's wiki:
+        http://flux.referata.com/
+"""
+
+def parse (token_stream):
+    pass
