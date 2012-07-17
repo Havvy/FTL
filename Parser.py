b4e9cf9f9cae7807070b2ef19a6eec576093d65b
r3 <ryan.roler@gmail.com>
2012-07-16 18:00:04 -0700

    Minor work on Parser

--- Parser.py
+++ Parser.py
@@ -8,8 +8,86 @@
         http://flux.referata.com/
 """
 
-from Lexer import (gk
+from Lexer import (AT, CLOSE_PAREN, COMMA, ESCAPED, EQUALS, NEW_LINE,
+                   OPEN_PAREN, PERIOD, TEXT, VAR, OPEN_LINK, CLOSE_LINK)
+
+"""
+FLUX
+  LINE
+    TEMPLATE
+    TEXT
+    LINK
+    LIST_ITEM
+      LINK
+      TEXT
+    VARIABLE
+"""
 
 
-def parse (token_stream):
+class Node():
+    legal_children = ('*')
+
+    def __init__(self):
+        self.children = []
+
+    @property
+    @classmethod
+    def name(cls):
+        return cls.__name__
+
+    @classmethod
+    def legal_child(cls, child):
+        return child in cls.legal_children
+
+    def add_child(self, child):
+        if self.legal_children(child):
+            self.children.append(child)
+
+
+class Flux(Node):
+    legal_children = ('Line')
+
+
+class Line(Node):
     pass
+
+
+class Template(Node):
+    legal_children = ()
+
+    def __init__(self, name, *args, **kwargs):
+        super()
+        self.args = {'name': name}
+
+        count = 1
+        for arg in args:
+            args[count] = arg
+
+        args.update(kwargs)
+
+
+class Link(Node):
+    legal_children = ()
+    pattern = (OPEN_LINK, '*', CLOSE_LINK)
+
+    def __init__(self, name, dest):
+        super()
+        self.name = name
+        self.destination = dest
+        # TODO: This isn't good enough:
+        self.local = dest.startswith('http://')
+
+
+class Text(Node):
+    legal_children = ()
+    pattern = (TEXT)
+
+    def __init__(self, content):
+        super()
+        self.content = content
+
+if __name__ == '__main__':
+    root = Flux()
+    line = Line()
+
+    root.add_child(line)
